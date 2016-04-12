ce5100f04d45ece9da3c83fa43368c58525e68a7
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-04-12 02:30:17 +0100

    rename utils to math
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- algpy/math.py
+++ algpy/math.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 class Fraction(object):
     """docstring for Fraction."""
 
-    def __init__(self, numerator, denominator):
+    def __init__(self, numerator, denominator=1, whole_number=0):
         """
         Constructor.
 
@@ -13,8 +13,18 @@ class Fraction(object):
             numerator (int): The numerator or top part of the fraction
             denominator (int): The denominator or bottom part of the fraction
         """
-        self.numerator = numerator
+        if denominator == 0:
+            raise ValueError("Denominator cannot be zero.")
+
+        if not isinstance(numerator, int) or not isinstance(denominator, int)\
+                or not isinstance(whole_number, int):
+            raise TypeError(
+                'Invalid argument type. All argument must be integrers')
+
+        self.top = (whole_number * denominator) + numerator
         self.denominator = denominator
+        self.whole = whole_number
+        self.numerator = numerator
 
     def __str__(self):
         """
@@ -23,7 +33,7 @@ class Fraction(object):
         Returns:
             name (TYPE): Formatted instance
         """
-        return "{} : {}/{}".format(self, self.top, self.bottom)
+        return "{0} {1}/{2}".format(self.whole, self.top, self.denominator)
 
     def __unicode__(self):
         """
@@ -32,4 +42,33 @@ class Fraction(object):
         Returns:
             name (TYPE): Formatted instance
         """
-        return "{} : {}/{}".format(self, self.top, self.bottom)
+        return "{0} {1}/{2}".format(self.whole, self.top, self.denominator)
+
+    def __add__(self, other):
+        """
+        Addition magic method.
+
+        Args:
+            other (Fraction): The other fraction to add
+
+        Returns:
+            name (Fraction): New fraction instance
+        """
+        denominator = self.denominator * other.denominator
+        numerator = (self.top * other.denominator) + \
+            (other.top * self.denominator)
+
+        whole = numerator // denominator
+        numerator = numerator % denominator
+        if not denominator % numerator:
+            denominator = denominator // numerator
+
+        return Fraction(numerator, denominator, whole)
+
+
+class Binary(object):
+    """docstring for Binary"""
+
+    def __init__(self, arg):
+        super(Binary, self).__init__()
+        self.arg = arg
